d7be0cec66252281d440a28626e9057f261c29c7
章小慢 <manyangz@jumei.com>
2017-11-14 16:13:22 +0800

    update test script

--- test.php
+++ test.php
@@ -3,7 +3,8 @@
 require "Crontab.php";
 
 $cron_string = $argv[1];
+$hide_past_sec = (boolean)$argv[2];
 var_export(date("Y-m-d H:i:s"));
 echo "\n";
-$result = Crontab::parse($cron_string, false);
+$result = Crontab::parse($cron_string, $hide_past_sec);
 var_export($result);
